1036b4b7689f02bc036a63df00ae4a2b52cf0a60
yakivkrustalov <zoomerrremover@gmail.com>
2025-04-09 07:04:52 -0400

    feat: Implemented basic authentication

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 from flask import Flask, request, Response
-
+users = {
+    "admin": 123,
+    "guest": 456
+}
 app = Flask(__name__)
 
+def authenticate():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return False
+    user = users.get(auth.username)
+    if not user or user != auth.password:
+        return False
+    return auth.username, auth.password
+
+def requires_auth(f):
+    """Decorator to enforce HTTP basic auth."""
+    from functools import wraps
+
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not authenticate():
+            return authenticate_response()
+        return f(*args, **kwargs)
+    return decorated
+
+def authenticate_response():
+    return Response(
+        'Could not verify your access!', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'}
+    )
+
 @app.route("/create")
+@requires_auth
 def create():
     return "<p>Create!</p>"
 
 @app.route("/update")
+@requires_auth
 def update():
     return "<p>Update!</p>"
 
@@ -15,5 +47,7 @@ def read():
     return "<p>READ!</p>"
 
 @app.route("/delete")
+@requires_auth
 def delete():
     return "<p>Delete!</p>"
+
