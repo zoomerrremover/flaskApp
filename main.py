5f54df8cbd9183297a8530b8a371c6b302384440
yakivkrustalov <zoomerrremover@gmail.com>
2025-04-09 06:34:51 -0400

    feat: Implemented basic CRUD

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+from flask import Flask, request, Response
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route("/create")
+def create():
+    return "<p>Create!</p>"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/update")
+def update():
+    return "<p>Update!</p>"
 
+@app.route("/read")
+def read():
+    return "<p>READ!</p>"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route("/delete")
+def delete():
+    return "<p>Delete!</p>"
