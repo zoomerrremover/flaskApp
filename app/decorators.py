456547e789df206ff04a0b1775dd0674d03c8d5f
yakivkrustalov <zoomerrremover@gmail.com>
2025-04-19 12:51:58 -0400

    feat: Added decorator for pydantic

--- app/decorators.py
+++ app/decorators.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from flask import requst, Response
 from authentication import authenticate
+from functools import wraps
+from pydantic.main import BaseModel, ValidationError
+
 
 def requires_auth(f):
-    from functools import wraps
 
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -12,3 +15,27 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+def validate_request(model: type[BaseModel]):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                data = model(**request.get_json())
+                return f(data, *args, **kwargs)
+            except ValidationError as e:
+                return jsonify({"error": e.errors()}), HTTPStatus.BAD_REQUEST
+        return wrapper
+    return decorator
+
+def serialize_response(model: type[BaseModel]):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            result = f(*args, **kwargs)
+            if isinstance(result, model):
+                return jsonify(result.model_dump())
+            elif isinstance(result, tuple) and isinstance(result[0], model):
+                return jsonify(result[0].model_dump()), *result[1:]
+            return result
+        return wrapper
+    return decorator
